012bf7a1705c1fc819fa146e54d211bc41a4a785
lpciccolella <lorianaciccolella@gmail.com>
2021-03-17 13:42:24 -0400

    fix bug

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,11 @@
 export default {
-    JWT_SECRET: process.env.JWT_SECRET,
-    jwtSecret: process.env.JWT_SECRET , 
-    secretOrKey: process.env.JWT_SECRET,
+    JWT_SECRET: process.env.JWT_SECRET || "somesecrettoken",
+    jwtSecret: process.env.JWT_SECRET || "somesecrettoken", 
+    secretOrKey: process.env.JWT_SECRET || "somesecrettoken",
 
     DB: {
-        URI: process.env.MONGODB_URI,
-        USER: process.env.MONGODB_USER,
-        PASSWORD: process.env.MONGODB_PASSWORD,
+        URI: process.env.MONGODB_URI || "mongodb+srv://lciccolella:lciccolella@twitterclonecluster.bsdlx.mongodb.net/dbname?retryWrites=true&w=majority ",
+        USER: process.env.MONGODB_USER  || "lciccolella",
+        PASSWORD: process.env.MONGODB_PASSWORD || "lciccolella",
     }
 }
